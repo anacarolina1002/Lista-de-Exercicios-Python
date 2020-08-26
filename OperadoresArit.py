fbf859ab2e6de17cd2d2160d3d792b8b2d8e09f7
Anacarolina1002 <carolquaglioto@outlook.com>
2020-08-26 16:33:54 -0300

    :rocket: atividade aula 26/08

---
+++ OperadoresArit.py
@@ -0,0 +1,70 @@
+#Variáveis
+numeroX = 2
+numeroY = 3
+
+#Operadores Aritméticos
+#Soma
+resposta = numeroX + numeroY
+#Mostrar a resposta método 1
+print("A resposta equivale a:",resposta)
+#Mostrar a resposta método 2
+print("A resposta equivale a:",(numeroX + numeroY))
+#No método acima, não se pode usar o resultado em
+#outros comandos, pois o valor não foi atribuido a
+#uma variável específica
+
+#Subtração
+respostaSubtracao = numeroX - numeroY
+#Mostrar a resposta método 1
+print("O valor da subtração é:",respostaSubtracao)
+#Mostrar a resposta método 2
+print("O valor da subtração é:", (numeroX - numeroY))
+#No método acima, não se pode usar o resultado em
+#outros comandos, pois o valor não foi atribuido a
+#uma variável específica
+
+#Multiplicação
+respostaMultiplicacao = numeroX * numeroY
+#Mostrar a resposta método 1
+print("O valor da multiplicação é:",respostaMultiplicacao)
+#Mostrar a resposta método 2
+print("O valor da multiplicação é:", (numeroX * numeroY))
+#No método acima, não se pode usar o resultado em
+#outros comandos, pois o valor não foi atribuido a
+#uma variável específica
+#Mostrar a resposta método 3 (Preguiça)
+meuTexto = "A resposta da multiplicação é:"
+print(meuTexto,(numeroX * numeroY))
+
+#Divisão
+respostaDivisao = numeroX / numeroY
+#Mostrar a resposta método 1
+print("O valor da divisão é:",respostaDivisao)
+#Mostrar a resposta método 2
+print("O valor da divisão é:", (numeroX / numeroY))
+#No método acima, não se pode usar o resultado em
+#outros comandos, pois o valor não foi atribuido a
+#uma variável específica
+
+#Expoente
+respostaExpoente = numeroX ** numeroY
+#Mostrar a resposta método 1
+print("O valor do expoente é:",respostaExpoente)
+#Mostrar a resposta método 2
+print("O valor do expoente é:", (numeroX ** numeroY))
+#No método acima, não se pode usar o resultado em
+#outros comandos, pois o valor não foi atribuido a
+#uma variável específica
+
+#Operadores Lógicos e Relacionais
+print("Operador lógico AND(e)",(numeroX <5 and numeroY < 10))
+print("Operador lógico OR(ou)",(numeroX < 5 or numeroY > 20))
+print("Operador lógico de NEGAÇÃO(não)", (not(numeroX < 5 and numeroY < 10)))
+print("Operador relacional != (diferente):",(numeroX != numeroY))
+
+armazenaValorTeclado = int(input("Digite um valor:"))
+print("O valor digitado foi:",type(armazenaValorTeclado))
+
+
+
+
